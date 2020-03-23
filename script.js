eb344ce3afc70b123ea92a93b8896a18e8d72e0e
Khaliun880818 <61443274+Khaliun880818@users.noreply.github.com>
2020-03-23 20:45:05 +0800

    .

---
+++ script.js
@@ -0,0 +1,25 @@
+let randomaudio = document.getElementById('musicc')
+let song2 = document.getElementById('musicc2')
+function songplay(){
+randomaudio.play()
+song2.pause()
+document.getElementById('ment1').style.display = "none"
+document.getElementById('ment2').style.display = "block"
+document.getElementById('ment3').style.display = "block"
+document.getElementById('ment4').style.display = "none"
+//document.getElementById('circ1').style.display = "none"
+//document.getElementById('circ2').style.display = "none"
+//document.getElementById('circ3').style.display = "none"
+//document.getElementById('circ4').style.display = "none"
+//document.getElementById('line').style.display = "none"
+
+}
+function song2play(){
+randomaudio.pause()
+song2.play()
+document.getElementById('ment5').style.display = "block"
+document.getElementById('ment2').style.display = "none"
+document.getElementById('ment3').style.display = "none"
+document.getElementById('ment4').style.display = "block"
+
+}
---
+++ sketch.js
@@ -0,0 +1,32 @@
+let angle = 0;
+let r;
+let b;
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	angleMode(DEGREES);
+  r = color('#BDC2EA');
+  b = color('#FCC7E9');
+
+}
+function draw(){
+  let t = map(mouseX,0,width,0,1.0);
+  let c = lerpColor(r,b,t);
+	fill('#9D9A9B');
+	push();
+	translate(windowWidth/2, windowHeight/2);
+	rotate(angle);
+	fill(c);
+	strokeWeight(1);
+	circle(0, 0, 500);
+	stroke('black');
+	strokeWeight(4);
+  line(0, 0, 0, 250);
+  pop();
+	circle(windowWidth/2, windowHeight/2, 125)
+	fill('#F3E9EE');
+	circle(windowWidth/2, windowHeight/2, 110);
+	fill('#C3EAE3');
+	circle(windowWidth/2, windowHeight/2, 100);
+  angle = angle +1;
+
+	}
